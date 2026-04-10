b6557d316d53186fe812259022ba905afba60385
Yasmin <yasmin.porto@aluno.senai.br>
2026-04-10 16:16:06 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,46 @@
+import 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import HomeScreen from './src/screens/HomeScreen';
+import PerfilScreen from './src/screens/PerfilScreen';
+import ConfigScreen from './src/screens/ConfigScreen';
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <NavigationContainer>
+                <Drawer.Navigator
+                    initialRouteName="Home"
+                    screenOptions={{
+                        headerStyle: {
+                            backgroundColor: '#1e40af',
+                        },
+                        headerTintColor: '#fff',
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+                        },
+                        drawerActiveTintColor: '#1e40af',
+                        drawerActiveBackgroundColor: '#dbeafe',
+                        drawerInactiveTintColor: '#334155',
+                        drawerLabelStyle: {
+                            fontSize: 16,
+                            fontWeight: '500',
+                        },
+                        drawerStyle: {
+                            backgroundColor: '#f8fafc',
+                            width: 260,
+                        },
+                    }}
+                >
+                    <Drawer.Screen name="Home" component={HomeScreen} />
+                    <Drawer.Screen name="Perfil" component={PerfilScreen} />
+                    <Drawer.Screen name="Configurações" component={ConfigScreen} />
+                </Drawer.Navigator>
+            </NavigationContainer>
+        </GestureHandlerRootView>
+    );
+}
